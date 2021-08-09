8ed18a84d7e840e97c21bfcb45bc1b8d12f09d18
Maxime CASTILLO <maxime.castillo@gmail.com>
2021-08-09 15:00:04 +0200

    Ajoute nom planète d'origine au perso et l'affiche

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,6 +10,10 @@ class CharactersController < ApplicationController
         character = get(people_url)
         filtered_characters.push(character) if (character['mass'].to_f > 75)
       end
+      filtered_characters.each do |character|
+        homeworld_name = get(character['homeworld'])['name']
+        character['homeworld_name'] = homeworld_name
+      end
       @data.push({
         'film_title' => film['title'],
         'filtered_characters' => filtered_characters
