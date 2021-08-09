d33ac104a1147f31f0c00c1a99583568c3483176
Maxime CASTILLO <maxime.castillo@gmail.com>
2021-08-09 15:52:38 +0200

    Ajoute Bootstrap, clean controller, améliore view

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,23 +3,24 @@ class CharactersController < ApplicationController
     @data = []
 
     films = get("http://5.196.14.171:8000/api/films")['results']
-    puts "$"*100
+
     films.each do |film|
       filtered_characters = []
+
       film['characters'].each do |people_url|
         character = get(people_url)
         filtered_characters.push(character) if (character['mass'].to_f > 75)
       end
+
       filtered_characters.each do |character|
         homeworld_name = get(character['homeworld'])['name']
         character['homeworld_name'] = homeworld_name
       end
+      
       @data.push({
         'film_title' => film['title'],
         'filtered_characters' => filtered_characters
       })
     end
-    puts @data.class
-    p @data
   end
 end
