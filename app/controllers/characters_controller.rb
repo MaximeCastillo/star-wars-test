f352e144abd4c71ddbf5a37a4baf68bbcc9aa1f2
Maxime CASTILLO <maxime.castillo@gmail.com>
2021-08-09 14:45:37 +0200

    Récupère et affiche les personnages triés+groupés

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,18 @@
 class CharactersController < ApplicationController
   def index
     @data = []
-    
+
     films = get("http://5.196.14.171:8000/api/films")['results']
     puts "$"*100
     films.each do |film|
+      filtered_characters = []
+      film['characters'].each do |people_url|
+        character = get(people_url)
+        filtered_characters.push(character) if (character['mass'].to_f > 75)
+      end
       @data.push({
         'film_title' => film['title'],
-        'characters' => film['characters']
+        'filtered_characters' => filtered_characters
       })
     end
     puts @data.class
